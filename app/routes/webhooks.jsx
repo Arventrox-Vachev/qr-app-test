dd15d0c6d522fa4516d492c3a6b92a6526032590
Stiliyan Ivanov <stiliyan@alphabite.io>
2023-08-24 14:06:44 +0300

    testing #2

--- app/routes/webhooks.jsx
+++ app/routes/webhooks.jsx
@@ -1,10 +1,8 @@
 import { authenticate } from "../shopify.server";
 import db from "../db.server";
-import { json } from "@remix-run/node";
 
 export const action = async ({ request }) => {
   const { topic, shop, session } = await authenticate.webhook(request);
-  const { admin } = await authenticate.admin(request);
 
   switch (topic) {
     case "APP_UNINSTALLED":
@@ -13,27 +11,6 @@ export const action = async ({ request }) => {
       }
       break;
     case "CUSTOMERS_DATA_REQUEST":
-      if (admin) {
-        const response = admin.graphql(`mutation {
-          webhookSubscriptionCreate(
-            topic: ORDERS_CREATE
-            webhookSubscription: {
-              format: JSON,
-              callbackUrl: "https://12345.ngrok.io/"}
-          ) {
-            userErrors {
-              field
-              message
-            }
-            webhookSubscription {
-              id
-            }
-          }
-        }`);
-        console.log(response);
-        return json({ response });
-      }
-
     case "CUSTOMERS_REDACT":
     case "SHOP_REDACT":
     default:
